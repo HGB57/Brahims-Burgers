753e6a8f804bad8777ef7583b933b2564f9e74c5
HGB57 <101357087+HGB57@users.noreply.github.com>
2022-03-10 13:45:30 -0500

    Check this

---
+++ BrahimsBurgers.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello! Welcome to Brahim's Burgers" << endl;
+	cout << "\nToday we have a special on Bacon Burgers. Buy 4 and get 2% off the next Bacon Burger you buy!"<<endl;
+}
