4934c6a5cd15076f6df9e8d0ae89517f73a332ba
HGB57 <heys.jonathan@gmail.com>
2022-03-10 13:57:03 -0500

    Now check this
    test 2: electric boogaloo

--- BrahimsBurgers.cpp
+++ BrahimsBurgers.cpp
@@ -5,4 +5,6 @@ int main()
 {
 	cout << "Hello! Welcome to Brahim's Burgers" << endl;
 	cout << "\nToday we have a special on Bacon Burgers. Buy 4 and get 2% off the next Bacon Burger you buy!"<<endl;
+
+	cout << "Please select a main dish" << endl;
 }
