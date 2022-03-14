d8f4a40f4caf7893cc8217dd920f1a5b1c181996
Derekscodeing <derekkoppelaar@gmail.com>
2022-03-14 18:41:03 -0400

    fdsa

--- BrahimsBurgers/BrahimsBurgers/BrahimsBurgers.cpp
+++ BrahimsBurgers/BrahimsBurgers/BrahimsBurgers.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Checking";
+	cout << "testing testing";
 	int firstchoice, burgerchoice;
 	cout << "Hello! Welcome to Brahim's Burgers" << endl;
 	cout << "\nToday we have a special on Bacon Burgers. Buy 4 and get 2% off the next Bacon Burger you buy!" <<endl;
