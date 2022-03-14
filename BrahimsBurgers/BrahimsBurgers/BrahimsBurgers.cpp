3e6cb82ac4be5f7e9fc46601eb56320f5a97fd03
Derekscodeing <derekkoppelaar@gmail.com>
2022-03-14 15:48:31 -0400

    Update BrahimsBurgers.cpp

--- BrahimsBurgers/BrahimsBurgers/BrahimsBurgers.cpp
+++ BrahimsBurgers/BrahimsBurgers/BrahimsBurgers.cpp
@@ -1,11 +1,59 @@
 #include <iostream>
-//hello there
+#include <iomanip>
+
 using namespace std;
 int main()
 {
+	cout << "Checking";
+	/*int firstchoice, burgerchoice;
 	cout << "Hello! Welcome to Brahim's Burgers" << endl;
-	cout << "\nToday we have a special on Bacon Burgers. Buy 4 and get 2% off the next Bacon Burger you buy!"<<endl;
+	cout << "\nToday we have a special on Bacon Burgers. Buy 4 and get 2% off the next Bacon Burger you buy!" <<endl;
+	do {
 
-	cout << "Please select a main dish" << endl;
+		cout << "Please select a main dish" << endl;
+		cout << "1. Burger" << endl;
+		cout << "2. Wrap" << endl;
+		cout << "3. Order complete" << endl;
+		cin >> firstchoice;
+		switch (firstchoice)
+		{
+
+		case 1:
+
+
+			do {
+				cout << "Which burger do you want?" << endl;
+				cout << "1. Bacon deluxe" << endl;
+				cout << "2. Based burger" << endl;
+				cin >> burgerchoice;
+					switch (burgerchoice)
+					{
+					case 1:
+
+
+
+
+
+
+					}
+			}
+			while (burgerchoice < 3 && burgerchoice >0)
+
+
+
+
+
+
+				break;
+		case 2:
+			break
+		case 3:
+			break;
+		}
+
+	}
+	while(fistchoice <=3 && firstchoice >0)
+
+
+// Dereks edit*/
 }
-// Dereks edit
